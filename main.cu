251117117077a0e2539fa0aa8607e6da893275e4
samtru99 <samtrujillo1999@gmail.com>
2024-02-23 12:38:22 -0800

    cleaned up file

--- main.cu
+++ main.cu
@@ -7,6 +7,7 @@
 #include <cuda_runtime.h>
 #include <device_launch_parameters.h>
 #include <string.h>
+#include "sequence.h"
 
 struct matrix
 {
@@ -22,15 +23,6 @@ struct pair_hash
     }
 };
 
-/*
-std::size_t unordered_map_hashing1(const std::pair<int, int>& p) {
-    auto h1 = std::hash<int>{}(p.first);
-    auto h2 = std::hash<int>{}(p.second);
-
-    // A simple combination hash function
-    return h1 ^ h2;
-};
-*/
 int dp(int i, int j, std::vector<int> dimensions, std::vector<std::vector<int>> *s_table,  std::unordered_map<std::pair<int,int>,int, pair_hash> *seen)
 {
     /*
@@ -81,7 +73,7 @@ void init_dp(std::vector<std::vector<int>> *s_table, std::vector<matrix> list_ma
     }
 
     /*
-        Get all dimenisons
+        Extract D_0 -> D_(N-1) dimenison values
     */
     std::vector<int> dimenions;
     for(int i = 0; i < list_matrixes.size(); i++)
@@ -99,24 +91,9 @@ void init_dp(std::vector<std::vector<int>> *s_table, std::vector<matrix> list_ma
             dimenions.push_back(a);
         }
     }
-    /*
-    for(int i = 0; i < dimenions.size(); i++)
-    {
-        std::cout << dimenions[i] << " \n " << std::endl;
-    }
-    */
     dp(1,list_matrixes.size(), dimenions, s_table, &seen);
-    std::cout << "max is " << seen[{1,4}] << std::endl;
+    std::cout << "MAX is " << seen[{1,list_matrixes.size()}] << std::endl;
 
-    std::cout << "S TABLE: " << std::endl;
-    for(int i = 0; i < s_table->size(); i++)
-    {
-        for(int j = 0; j < s_table[0].size(); j++)
-        {
-            std::cout << (*s_table)[i][j] << "\t";
-        }
-        std::cout << std::endl;
-    }
 }
 
 int main()
@@ -125,37 +102,67 @@ int main()
     matrix A2;
     matrix A3;
     matrix A4;
-    A1.dimension = std::make_tuple(5,4);
-    A2.dimension = std::make_tuple(4,6);
-    A3.dimension = std::make_tuple(6,2);
-    A4.dimension = std::make_tuple(2,7);
+    matrix A5;
+    
+   
+    A1.dimension = std::make_tuple(4,10);
+    A2.dimension = std::make_tuple(10,3);
+    A3.dimension = std::make_tuple(3,12);
+    A4.dimension = std::make_tuple(12,20);
+    A5.dimension = std::make_tuple(20,7);
 
+   
+   /*
+   A1.dimension = std::make_pair(5,4);
+   A2.dimension = std::make_pair(4,6);
+   A3.dimension = std::make_pair(6,2);
+   A4.dimension = std::make_pair(2,7);
+   */
 
-
-    /*
-        Create M and S tables
-    */
-    std::vector<std::vector<int>> s_table;
-
-    int rows = 4;
-    int columns = 4;
-    for(int i = 0; i < rows; i++)
-    {
-        std::vector<int> row(columns, 0);
-        s_table.push_back(row);
-    }
-    /*
-        Initialize m_table to set self X self to be 0
-    */
-
-    /*
-        Extract D_0 -> D_(N-1) values
-    */
     std::vector<matrix> list_matrixes;
     list_matrixes.push_back(A1);
     list_matrixes.push_back(A2);
     list_matrixes.push_back(A3);
     list_matrixes.push_back(A4);
+    list_matrixes.push_back(A5);
+
+
+    /*
+        Create S table and initalize diagonal to zeros
+    */
+    std::vector<std::vector<int>> s_table;
+    int rows = list_matrixes.size();
+    int columns = list_matrixes.size();
+    for(int i = 0; i < rows; i++)
+    {
+        std::vector<int> row(columns, 0);
+        s_table.push_back(row);
+    }
+
+    /*
+        Perform DP
+    */
     init_dp(&s_table, list_matrixes);
+
+    //Print S-Table for debugging
+    std::cout << "S TABLE: " << std::endl;
+    for(int i = 0; i < s_table.size(); i++)
+    {
+        for(int j = 0; j < s_table[0].size(); j++)
+        {
+            std::cout << s_table[i][j] << "\t";
+        }
+        std::cout << std::endl;
+    }
+    
+    std::cout << "beginning the sequence " << std::endl;
+    Sequence seq(s_table);
+    Node* root = new Node();
+    std::cout << "processing " << std::endl;
+    root = seq.init_sequence(1, list_matrixes.size());
+    std::cout << "printing " << std::endl;
+    std::cout << "(" << " ";
+    seq.print_sequence(root);
+    std::cout << ")" << " \n";
     return 0;
 }
