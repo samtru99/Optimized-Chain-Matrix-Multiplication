f9a1e8d60b85a47e437ddc26da290e8cb9cb8c72
samtru99 <samtrujillo1999@gmail.com>
2024-02-22 19:25:25 -0800

    sequence function works - need to test on even number of matrices

--- sequence.cpp
+++ sequence.cpp
@@ -1,44 +1,55 @@
 #include "stdlib.h"
 #include "sequence.h"
 
-Sequence::Sequence(std::vector<std::vector<int>> s_table,std::vector<int> matrix_sequence)
+
+Sequence::Sequence(std::vector<std::vector<int>> temp_table) : s_table(temp_table)
 {
-    s_table = s_table;
-    matrix_sequence = matrix_sequence;
+    std::cout << "Constructor executed. \n";
 }   
 
-Node Sequence::init_sequence(int i, int j)
+
+Node* Sequence::init_sequence(int i, int j)
 {
     /*
         Base cases
     */
     if(i > j)
     {
-        return;
+        return nullptr;
     }
+    Node* n = new Node();
     if(i == j-1)
     {
-        Node n;
-        n.left = nullptr;
-        n.right = nullptr;
-        n.seq = static_cast<char*>(malloc(3 * sizeof(char)));
-        n.seq[0] = '0' + i;
-        n.seq[1] = '0' + j;
-        n.seq[2] = '\0';
+        n->left = nullptr;
+        n->right = nullptr;
+        n->seq = static_cast<char*>(malloc(3 * sizeof(char)));
+        n->seq[0] = '0' + i;
+        n->seq[1] = '0' + j;
+        n->seq[2] = '\0';
+        return n;
     }
     if(i == j)
     {
-        Node n;
-        n.left = nullptr;
-        n.right = nullptr;
-        n.seq = static_cast<char*>(malloc(2 * sizeof(char)));
-        n.seq[0] = '0' + i + '\0';
+        //Node n;
+        n->left = nullptr;
+        n->right = nullptr;
+        n->seq = static_cast<char*>(malloc(2 * sizeof(char)));
+        n->seq[0] = '0' + i + '\0';
+        return n;
     }
-    int sub_sequence = s_table[i][j];
-    Node n;
-    n.left = init_sequence(i, sub_sequence);
-    n.right = init_sequence(sub_sequence+1, j);
+    int sub_sequence = s_table[i][j-1];
+    n->seq = static_cast<char*>(malloc((j-i+1) * sizeof(char)));
+    for(int x = 0; x <= (j-i); x++)
+    {
+        n->seq[x] = '0' + (x + i);
+    }
+    n->seq[j-i+1] = '\0';
+    n->left = init_sequence(i, sub_sequence);
+    n->right = init_sequence(sub_sequence+1, j);
     return n;
-
 }
 
+void Sequence::print_sequence(Node* n)
+{
+    return;
+}
