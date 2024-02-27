318e3a3a37b83733d9ced283d7aeacc16c807591
samtru99 <samtrujillo1999@gmail.com>
2024-02-27 15:59:49 -0800

    moved functions from main to their own file

--- Makefile
+++ Makefile
@@ -2,16 +2,20 @@
 #	nvcc main.cu -o main  -lcudart -lcudadevrt
 CXX = nvcc
 CPP = gcc
-CFLAGS = -std=c++17 -g
+CFLAGS = -std=c++11 -g
 LDFLAGS =  -lcudart -lcudadevrt
 
-main: main.o sequence.o
-	$(CXX) $(CFLAGS) -o main main.o sequence.o $(LDFLAGS)
+main: main.o sequence.o helper_functions.o
+	$(CXX) $(CFLAGS) -o main main.o sequence.o helper_functions.o $(LDFLAGS)
 
 main.o: main.cu
 	$(CXX) $(CFLAGS) -c main.cu
+	
 sequence.o: sequence.cpp
-	$(CPP) $(CFLAGS) -c sequence.cpp
+	$(CXX) $(CFLAGS) -c sequence.cpp
+
+helper_functions.o: helper_functions.cpp
+	$(CXX) $(CFLAGS) -c helper_functions.cpp
 
 clean:
 	rm -f main main.o sequence.o
---
+++ helper_functions.h
@@ -0,0 +1,28 @@
+#ifndef _HELPER_FUNCTIONS_H
+#define _HELPER FUNCTIONS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <tuple>
+#include <vector>
+#include <unordered_map>
+#include "sequence.h"
+
+struct pair_hash 
+{
+    std::size_t operator () (const std::pair<int,int> &p) const
+    {
+        return std::hash<int>{}(p.first) ^ std::hash<int>{}(p.second);
+    }
+};
+
+int dp(int i, int j, std::vector<int> dimensions, 
+    std::vector<std::vector<int>> *s_table,  
+    std::unordered_map<std::pair<int,int>,int, pair_hash> *seen);
+
+void setValues(matrix *x,int value);
+
+void init_dp(std::vector<std::vector<int>> *s_table, std::vector<matrix> list_matrixes);
+
+
+#endif
